4ad33526fc39d985264973ef73b8f80b21e2482f
Mantas Tarutis <mantas.tarutis@gmail.com>
2019-05-20 06:47:52 +0300

    fixing jenkins build error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// directory where compiled assets will be stored
+
+    .copyFiles({
+    from: './assets',
+
+    // optional target path, relative to the output dir
+    to: 'images/[path][name].[ext]',
+
+    // if versioning is enabled, add the file hash too
+    to: 'images/[path][name].[hash:8].[ext]',
+
+    // only copy files matching this pattern
+    pattern: /\.(png|jpg|jpeg|svg)$/
+    })
+
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
@@ -20,7 +34,7 @@ Encore
     .addEntry('downloadEditedText', './assets/js/downloadEditedText.js')
     .addEntry('app', './assets/js/app.js')
     .addEntry('highlightHardWords', './assets/js/highlightHardWords.js')
-    .addEntry('logo', './assets/quill.svg')
+    // .addEntry('logo', './assets/quill.svg')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
